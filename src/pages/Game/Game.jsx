ab95ef512ae0ec6e8d55a675d1d227f0badb8837
carlalopesj <carlaljovito@gmail.com>
2024-08-02 23:33:47 -0300

    feat: game page

---
+++ src/pages/Game/Game.jsx
@@ -0,0 +1,11 @@
+function Game() {
+    console.log("Game component rendered");
+    return (
+        <div>
+            <h1>OLAAA</h1>
+            <p>QUE E</p>
+        </div>
+    )
+}
+
+export default Game;
