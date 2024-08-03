97d94a4dc1c9e57bc4790c646e97dc05558fe950
carlalopesj <carlaljovito@gmail.com>
2024-08-03 00:43:34 -0300

    feat: game page

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,9 +1,34 @@
+import './Game.css'
+import Header from '../../components/Header/Header';
+import Cards from '../../components/Cards/Cards';
+import ImgLoja from '../../assets/fundo-loja-clara.jpg';
+import ImgArena from '../../assets/fundo-arena-clara.jpg';
+import ImgMissao from '../../assets/fundo-missoes-clara.jpg';
+
 function Game() {
-    console.log("Game component rendered");
     return (
-        <div>
-            <h1>OLAAA</h1>
-            <p>QUE E</p>
+        <div className='game-page'>
+            <Header />
+            <div className="destiny-page">
+                <h1> Qual será o seu destino? </h1>
+                <div className='destiny-container'>
+                    <Cards 
+                    type="Loja"
+                    img={ImgLoja}
+                    name="Feira"
+                    />
+                    <Cards 
+                    type="Arena"
+                    img={ImgArena}
+                    name="Ringue"
+                    />
+                    <Cards 
+                    type="Missões"
+                    img={ImgMissao}
+                    name="Extra"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
