cbf34391e564b6ae76061f87c324794f1798b9f5
carlalopesj <carlaljovito@gmail.com>
2024-08-02 23:33:01 -0300

    feat: character cards

---
+++ src/components/Characters/Characters.jsx
@@ -0,0 +1,19 @@
+import './Characters.css';
+import ImgArc from '../../assets/Arc.jpg';
+import { Link } from 'react-router-dom';
+function Characters(props) {
+
+    return (
+        <div className='character-container'>
+            <h2>{props.type}</h2>
+            <img src={props.img} className='img-character'/>
+            <div class="infos">
+                <p>Saúde: {props.health}</p>
+                <p>Prata: {props.gold}</p>
+            </div>
+            <button><Link to='/game' className='link'>{props.name}</Link></button>
+        </div>
+    );
+}
+
+export default Characters;
