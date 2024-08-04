59a5bb59f355ab1f854384363fb85c80378d4c53
carlalopesj <carlaljovito@gmail.com>
2024-08-04 18:12:20 -0300

    feat: game main page

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -6,27 +6,27 @@ import ImgArena from '../../assets/fundo-arena-clara.jpg';
 import ImgMissao from '../../assets/fundo-missoes-clara.jpg';
 
 function Game() {
+
+    const locals = [
+        {id: 1, type: "Loja", image: ImgLoja, name: "Feira"},
+        {id: 2, type: "Arena", image: ImgArena, name: "Ringue"},
+        {id: 3, type: "Missões", image: ImgMissao, name: "Extra"}
+    ]
+
     return (
         <div className='game-page'>
             <Header />
             <div className="destiny-page">
                 <h1> Qual será o seu destino? </h1>
                 <div className='destiny-container'>
-                    <Cards 
-                    type="Loja"
-                    img={ImgLoja}
-                    name="Feira"
-                    />
-                    <Cards 
-                    type="Arena"
-                    img={ImgArena}
-                    name="Ringue"
-                    />
-                    <Cards 
-                    type="Missões"
-                    img={ImgMissao}
-                    name="Extra"
-                    />
+                    {locals.map((local) => {
+                        return <Cards 
+                            key={local.id}
+                            type={local.type}
+                            img={local.image}
+                            name={local.name}
+                        />
+                    })}
                 </div>
             </div>
         </div>
