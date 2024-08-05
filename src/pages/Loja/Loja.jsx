0155d48eec85713ada2235d8f5a3898864087263
carlalopesj <carlaljovito@gmail.com>
2024-08-05 11:47:13 -0300

    feat: health function

--- src/pages/Loja/Loja.jsx
+++ src/pages/Loja/Loja.jsx
@@ -3,14 +3,27 @@ import Cards from "../../components/Cards/Cards";
 import ImgHealth from '../../assets/saude-pixel.png';
 import ImgXp from '../../assets/xp-pixel.png';
 import ImgSword from '../../assets/espada-pixel.png';
+import { useContext } from "react";
+import { PlayerContext } from "../../Context";
 
 function Loja() {
 
+    const { playerData, setPlayerData } = useContext(PlayerContext);
+
     const items = [
-        {id: 1, type: "Saúde", image: ImgHealth, name: "+10 Saúde"},
-        {id: 2, type: "XP", image: ImgXp, name: "+5 XP"},
+        {id: 1, type: "Saúde", image: ImgHealth, name: "+10 Saúde", actionBtn: addHealth},
+        {id: 2, type: "XP", image: ImgXp, name: "+5 XP" },
         {id: 3, type: "Arma", image: ImgSword, name: "Nova Arma"}
     ]
+
+    function addHealth() {
+        console.log("Health clicado");
+        setPlayerData((prevData) => ({
+            ...prevData,
+            health: prevData.health + 10,
+            gold: prevData.gold - 10 // supondo que a ação de adicionar saúde custa 10 de ouro
+        }));
+    }
 
     return (
         <div className='game-page'>
@@ -24,6 +37,7 @@ function Loja() {
                             type={item.type}
                             img={item.image}
                             name={item.name}
+                            handleClick={item.actionBtn}
                             
                         />
                     })}
